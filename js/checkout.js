ace084e0f7712baa25337b796ea83c0e575fdf41
Pedro Aragão <pedro.aragao.leite@gmail.com>
2024-02-01 17:44:50 +0100

    finish the shop and checkout

--- js/checkout.js
+++ js/checkout.js
@@ -3,26 +3,94 @@
 function validate() {
 	var error = 0;
 	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fEmail = document.getElementById("fEmail");
+	const fName = document.getElementById("fName");
+	const fEmail = document.getElementById("fEmail");
+	const fAddress = document.getElementById("fAddress");
+	const fLastN = document.getElementById("fLastN");
+	const fPassword = document.getElementById("fPassword");
+	const fPhone = document.getElementById("fPhone");
 
 	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
-	
+	const errorName = document.getElementById("errorName");
+	const errorEmail = document.getElementById("errorEmail");
+	const errorAddress = document.getElementById("errorAddress");
+	const errorLastN = document.getElementById("errorLastN");
+	const errorPassword = document.getElementById("errorPassword");
+	const errorPhone = document.getElementById("errorPhone");
+
+
+	// Form validations inputs
+	const letters = /^[A-Za-z]+$/;
+	const letterNumber = /^[0-9a-zA-Z]+$/;
+	const emailFormat =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/;
+	const phoneNumber = /^\d{9}$/;
+	const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9]).+$/;
+
 	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
+
+	// Name validation
+	if (fName.value === "" || fName.value.length <= 2 || letters.test(fName.value) == false) {
+		errorName.style.display = 'block';
 		error++;
+	} else {
+		errorName.style.display = 'none';
 	}
 
-	if(fEmail.value == ""){
+	// Email validation
+	if (fEmail.value === "" || emailFormat.test(fEmail.value) == false) {
+		console.log(emailFormat.test(fEmail));
+		errorEmail.style.display = 'block';
 		error++;
+	} else {
+		errorEmail.style.display = 'none';
 	}
-	 
-	if(error>0){
+
+	// Address validation
+	if (fAddress.value === "" || fAddress.value.length <= 2) {
+		errorAddress.style.display = 'block';
+		error++;
+	} else {
+		errorAddress.style.display = 'none';
+	}
+
+	// LastName validation
+	if (fLastN.value === "" || fLastN.value.length <= 2 || letters.test(fLastN.value) == false) {
+		errorLastN.style.display = 'block';
+		error++;
+	} else {
+		errorLastN.style.display = 'none';
+	}
+
+	// Password validation
+	if (fPassword.value === "" || passwordRegex.test(fPassword.value) == false) {
+		console.log(passwordRegex.test(fPassword.value));
+		console.log(fPassword.value);
+		errorPassword.style.display = 'block';
+		error++;
+	} else {
+		console.log(passwordRegex.test(fPassword.value));
+		errorPassword.style.display = 'none';
+	}
+	// Phone validation
+	if (fPhone.value === "" || fPhone.value.length < 9 || phoneNumber.test(fPhone.value) == false) {
+		errorPhone.style.display = 'block';
+		error++;
+	} else {
+		errorPhone.style.display = 'none';
+	}
+
+	if (error > 0) {
 		alert("Error");
-	}else{
+	} else {
 		alert("OK");
 	}
 
+
+	const form = document.querySelector("form");
+	form.addEventListener("submit", (e) => {
+		e.preventDefault();
+	});
+
+	console.log(error);
 }
--- js/shop.js
+++ js/shop.js
@@ -74,30 +74,89 @@ var cart = [];
 
 var total = 0;
 
+const myCart = document.getElementById('cart_list');
+const totalPrice = document.getElementById('total_price');
+const cart_list = document.getElementById('cart_list');
+const count = document.getElementById('count_product');
+
 // Exercise 1
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cart array
+
+    // get the producxt from array by id
+    const productFound = products.find(products => products.id === id);
+    console.log(productFound);
+
+    // search in cart array if product already in the cart by id
+    const findCart = cart.find(product => product.id === productFound.id);
+
+    // if found push to cart and add quantity key and value 
+    if (!findCart) {
+        cart.push({ ...productFound, quantity: 1 });
+        count.innerHTML++;
+        // if in cart sum quantity by in the product cart
+    } else {
+        console.log(productFound);
+        findCart.quantity += 1;
+        count.innerHTML++;
+    }
+
+    console.log(cart);
+
 }
 
 // Exercise 2
 function cleanCart() {
-
+    cart = [];
+    myCart.innerHTML = "";
+    totalPrice.innerHTML = 0;
+    count.innerHTML = 0;
 }
 
 // Exercise 3
 function calculateTotal() {
+    total = 0;
     // Calculate total price of the cart using the "cartList" array
+    for (let i = 0; i < cart.length; i++) {
+        // use the subTotal key created in the applyPromotionsCart() to calculate the total
+        total += cart[i].subTotal;
+    }
+    return total;
 }
 
 // Exercise 4
 function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
+
+    for (let item of cart) {
+        // created a subTotal to calculate price * quantity
+        item.subTotal = item.price * item.quantity;
+        if (item.id === 3 && item.quantity >= 10) {
+            // calculate the subTotal with the discount
+            item.subTotal = item.subTotal - ((item.price * item.offer.percent) / 100 * item.quantity);
+        } else if (item.id === 1 && item.quantity >= 3) {
+            item.subTotal = item.subTotal - ((item.price * item.offer.percent) / 100 * item.quantity);
+        }
+    }
 }
 
 // Exercise 5
 function printCart() {
+    applyPromotionsCart();
     // Fill the shopping cart modal manipulating the shopping cart dom
+    totalPrice.innerHTML = 0;
+    cart_list.innerHTML = '';
+    cart.forEach(item => {
+        cart_list.innerHTML += `<tr>
+    <th scope="row">${item.name}</th>
+    <td class="price">${item.price}</td>
+    <td class="qty">${item.quantity}</td>
+    <td>$ ${item.subTotal}</td>
+    <td><button class="btn btn-outline-dark" onclick="removeFromCart(${item.id})"><i class="fa fa-trash-can"></i></button></td>
+    </tr>`
+    })
+    totalPrice.innerHTML = calculateTotal().toFixed(2);
 }
 
 
@@ -105,7 +164,24 @@ function printCart() {
 
 // Exercise 7
 function removeFromCart(id) {
+    // check for the id index in the cart array of objs / will return -1 the item doesnt exist or will return the index
+    const findIndex = cart.findIndex(product => product.id === id);
 
+    // if the product quantity its equal to 1 will remove it from the array
+    // i call the printCart() to update the prices
+    if (cart[findIndex].quantity === 1) {
+        cart.splice(cart[findIndex], 1);
+        count.innerHTML--;
+        // printCart();
+
+        // if the quantity its greater than 1 will reduce by 1 the quantity until its equal to 1
+        // i call the printCart() to update the prices
+    } else if (cart[findIndex].quantity > 1) {
+        cart[findIndex].quantity -= 1;
+        count.innerHTML--;
+        // printCart()
+    }
+    printCart();
 }
 
 function open_modal() {
